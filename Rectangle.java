53d1bc8d569be440fe4e1feebfacc4fa97188126
Elf <elvirius.gr@gmail.com>
2025-03-06 18:56:40 +0200

    Commit add  Rectangle

--- Rectangle.java
+++ Rectangle.java
@@ -1,2 +1,13 @@
 public class Rectangle {
+    private int sideA;
+    private int sideB;
+
+    public Rectangle(int sideA, int sideB) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+    }
+
+    public static int calcArea(){
+        return sideA*sideB;
+    }
 }
