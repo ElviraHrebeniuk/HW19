45586e9e72148a16b10b12ded7b41c0e8a0b91d1
Elf <elvirius.gr@gmail.com>
2025-03-11 17:07:34 +0200

    Commit Figures 3

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,32 @@ public class Main {
         figures.add(rec1);
 
         for(Figures list : figures) System.out.println(list);
+        System.out.println("----------------");
+        System.out.println(allArea(figures));
+        System.out.println(findFigur(figures, new Circle("Circle", 6)));
+        System.out.println(findFigur(figures, new Circle("Circle", 5)));
+        System.out.println(findFiguresAreaGreater(figures, 10));
+    }
+
+    public static double allArea(List<Figures>  list){
+        double result=0;
+        for (int i = 0; i < list.size(); i++) {
+           result += list.get(i).calcArea();
+        }
+        return result;
+    }
+    public static boolean findFigur(List<Figures>  list, Figures figur){
+        for (int i = 0; i < list.size(); i++) {
+            if(list.get(i).equals(figur)) return true;
+        }
+        return false;
+    }
+    public static List<Figures> findFiguresAreaGreater(List<Figures> list, double area){
+        List<Figures> result = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).calcArea()>area)
+                result.add(list.get(i));
+        }
+        return result;
     }
 }
